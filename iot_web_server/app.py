b5b34f7a4a08491b5eaed6f90aca232a3230d1d9
Juan Carlos Juárez <43016726+jc-juarez@users.noreply.github.com>
2022-05-10 08:05:51 -0500

    Update app.py

--- iot_web_server/app.py
+++ iot_web_server/app.py
@@ -11,7 +11,7 @@ from update import *
 
 st.title("Computerized Control - Iot Water Valves System")
 
-# Total Number of People currently Inside
+# Current Water Limit of System
 
 current_water_limit = get_water_limit()
 
@@ -38,9 +38,7 @@ system_running = int(get_system_state())
 if(not system_running):
     if st.button('Start System'):
         set_current_water_limit(float(current_water_limit))
-        print("a " + str(float(current_water_limit)))
         start_system()
-        has_started_once = True
     else:
         st.success("The System Has Finished or has not been started yet. Press 'Start System' to begin.")   
 else:
@@ -48,10 +46,9 @@ else:
         stop_system()
     else:
         remaining_liters = get_liters()
-        remaining_ratio = float((float(remaining_liters) * 100.0) / float(get_current_water_limit()))
-        print(get_current_water_limit())
+        remaining_ratio = int((float(remaining_liters) * 100.0) / float(get_current_water_limit()))
         st.text("Remaining Liters for use: {0} Liters".format(remaining_liters))
-        my_bar = st.progress(int(remaining_ratio))
+        my_bar = st.progress(remaining_ratio)
 
 # ------------------------------
 # Re-Run App every half a second
